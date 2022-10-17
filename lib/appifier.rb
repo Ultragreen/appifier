d644b5bd81b11017c8c7f9c5f5d7c53231d2b0a6
= <=>
2022-10-18 00:41:47 +0200

    draft

--- lib/appifier.rb
+++ lib/appifier.rb
@@ -1,6 +1,33 @@
-require "Appifier/version"
+#require "Appifier/version"
 
 module Appifier
   class Error < StandardError; end
 
+  class Recurser
+
+    def initialize(path: )
+      @files = Dir.glob("#{path}/**/*").each do |file|
+        p 
+        File.rename file, file.gsub(/(%%.*%%)/, 'test')
+      end
+    end
+  end
+
+
+  class FoldersManager < Recurser
+
+    def initialize(path:)
+
+    end
+
+  end
+
+  class FilesManager
+  end
+
+
 end
+
+
+test = Appifier::Recurser::new path: "/tmp/test_appifier"
+pp test
