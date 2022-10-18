a277345fe4ba98b4e2956a3d261c42619fb5907f
Romain GEORGES <romain.georges@orange.com>
2022-10-18 15:14:10 +0200

    adding generator for folder

--- lib/appifier.rb
+++ lib/appifier.rb
@@ -1,33 +1,81 @@
 #require "Appifier/version"
 
+
+require 'thot'
+require 'fileutils'
+
+include Thot
 module Appifier
   class Error < StandardError; end
 
-  class Recurser
+  class Generator
 
-    def initialize(path: )
-      @files = Dir.glob("#{path}/**/*").each do |file|
-        p 
-        File.rename file, file.gsub(/(%%.*%%)/, 'test')
+      attr_reader :src_paths
+      attr_reader :src_files
+      attr_reader :src_folders
+
+      attr_reader :target_folders
+      attr_reader :target_files
+
+
+    def initialize(src_root: , target_root:)
+
+      @src_root = src_root
+      @target_root = target_root
+
+      @data = {appname: 'test'}
+
+      @src_paths = Dir.glob("#{@src_root}/**/*", File::FNM_DOTMATCH) 
+      @src_paths.delete_if {|file| file =~ /\/\.$/}
+      @src_folders = @src_paths.select{|item| File::directory? item }
+      @src_files = @src_paths.select{|item| File::file? item }
+
+    end
+
+
+    def generate(dry_run: false )
+      puts 'Running in dry_run' if dry_run
+      calculate
+      generate_folders dry_run: dry_run
+  
+    end
+
+    def calculate
+      calculate_folders
+    end
+
+    private
+
+    def generate_folders(dry_run:)
+      puts "Target path to create in #{@target_root} :"
+      @target_folders.each do |path|
+        action = (dry_run)? '[skipped]' : '[OK]'
+        FileUtils.mkdir_p "#{@target_root}/#{path}", noop: dry_run
+        puts "#{action} #{path}"
       end
     end
-  end
 
 
-  class FoldersManager < Recurser
-
-    def initialize(path:)
-
+    def calculate_folders
+      @target_folders = []
+      @src_folders.each do |folder| 
+        template = Template::new strict:false, 
+          list_token: @data.keys, 
+          template_content: folder.delete_prefix("#{@src_root}/" )
+        template.map(@data)
+        @target_folders.push template.output
+      end
     end
 
   end
 
-  class FilesManager
-  end
+
+   
 
 
 end
 
 
-test = Appifier::Recurser::new path: "/tmp/test_appifier"
-pp test
+test = Appifier::Generator::new src_root: "/tmp/appifier/skeleton", target_root: "/tmp/appifier"
+test.generate
+
